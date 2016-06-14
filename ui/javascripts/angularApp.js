0db1d93ece359e42b6e8e1486c8597a1b48ecf7f
Syed Ghazi <Ashhad@ashhads-mbp.genevatrading.com>
2016-06-14 17:17:29 -0500

    Cleaned up Layout. Fixed adding and not removing bug

--- ui/javascripts/angularApp.js
+++ ui/javascripts/angularApp.js
@@ -31,6 +31,7 @@ function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('home');
 
 }])
+
 assetsApp.factory('assets', function(){
 	var assets = {
         assets: [],
@@ -73,19 +74,23 @@ assetsApp.controller('MainCtrl', [
 '$http',
 '$window',
 '$location',
-function($scope, assets, $http, $window, $location) {
+'$timeout',
+function($scope, assets, $http, $window, $location, $timeout) {
+	$scope.state = {color: "default"};
 	$scope.sortType = 'host';
 	$scope.sortReverse = false;
 	$scope.search = '';
 	$scope.assets = assets.assets;
+	$scope.list_index = [];
 	$scope.this_id = $location.search()['id'];
 	$scope.acc_lists = {
+		admin_users: ["pkusch"],
 		admin_groups: ["infrastructure_sg", "support_sg", "networking_sg"],
 		env_tag: ["prod", "dev"],
-		allowed_groups: ["infrastructure_sg", "support_sg", "networking_sg"],
-		allowed_users: ["splunksvc"],
-		roles: ["admin", "nadmin"],
-		role : ["admin", "nadmin"]
+		allowed_groups: ["infrastructure_sg", "support_sg", "networking_sg", "kdb-users_sg", "risk_sg", "core-tech_sg", "pmg_sg", "cdg_sg", "armada_sg", "rhone-developers_sg", "gamc_sg", "house_sg"],
+		allowed_users: ["splunksvc", "pcapd", "rhonepcap", "rhouse", "coredevsvc", "scilasvc", "refdatasvc", "rhonemdssvc", "rhonesvc", "rhonemdsdevsvc"],
+		roles: ["admin", "nadmin", "kdb", "rhone", "mds"],
+		role : ["admin", "nadmin", "kdb", "rhone", "mds"]
 	};
 	$scope.existing_lists ={};
 
@@ -146,6 +151,7 @@ function($scope, assets, $http, $window, $location) {
 				}
 			}
 			for (var k in assets.this_pillar[0]){
+				$scope.list_index.push(k);
 				if (assets.this_pillar[0][k].constructor === Array){
 					for(var nested in assets.this_pillar[0][k]){
 						if ($scope.acc_lists[k].indexOf(assets.this_pillar[0][k][nested])>=0){
@@ -218,11 +224,24 @@ function($scope, assets, $http, $window, $location) {
 	    });
 	};
 
-	$scope.collectElems = function(item){
-		obj = JSON.parse(item);
-		$scope.index = obj[Object.keys(obj)[0]];
-		$scope.value = Object.keys(obj)[0];
-		$scope.limit = 0;
+	$scope.collectElems = function(item, removeFlag){
+		if(item != undefined){
+			obj = JSON.parse(item);
+			$scope.index = obj[Object.keys(obj)[0]];
+			//console.log($scope.index);
+			$scope.value = Object.keys(obj)[0].substr(0, Object.keys(obj)[0].indexOf(','));
+			$scope.key = Object.keys(obj)[0].substr(Object.keys(obj)[0].indexOf(',')+1);
+			$scope.limit = 0;
+		}
+		$scope.master_index = $scope.list_index.sort().indexOf($scope.key);
+		if(removeFlag===true){
+		    document.getElementById("addBut"+$scope.master_index).disabled = true;
+		    document.getElementById("remBut"+$scope.master_index).disabled = false;
+		}
+		if(removeFlag===false){
+			document.getElementById("remBut"+$scope.master_index).disabled = true;
+			document.getElementById("addBut"+$scope.master_index).disabled = false;
+		}
 	}
 
 	$scope.grabModalVals = function(key, val) {
@@ -240,12 +259,18 @@ function($scope, assets, $http, $window, $location) {
 
 	$scope.updateVal = function(key, val, index, value, addFlag, removeFlag){
 		if (addFlag === true) {
+			$scope.state = '{"info":$index%2==0,"danger":$index=={{master_index}}}';
+			$('#add option').attr('selected', null);
+			document.getElementById("rem").disabled = false;
 			if (value!= undefined && $scope.limit < 1 && $scope.existing_lists[key].indexOf(value)<0){
 				$scope.existing_lists[key].push(value);
 				$scope.acc_lists[key].splice($scope.acc_lists[key].indexOf(value), 1);
 				$scope.limit ++;
 			}
 		} else if (removeFlag === true) {
+			$scope.state = '{"info":$index%2==0,"danger":$index=={{master_index}}}';
+			$('#rem option').attr('selected', null);
+			document.getElementById("add").disabled = false;
 			if (index!= undefined && $scope.limit < 1){
 				$scope.existing_lists[key].splice(index, 1);
 				if ($scope.acc_lists[key].indexOf(value)<0){
@@ -255,6 +280,7 @@ function($scope, assets, $http, $window, $location) {
 			}
 		} else {
 			if($scope.value!=undefined){
+				$scope.state = '{"info":$index%2==0,"danger":$index=={{master_index}}}';
 				$scope.existing_lists[key] = $scope.value;
 				$scope.value = undefined;
 			}
@@ -297,7 +323,43 @@ function($scope, assets, $http, $window, $location) {
 	}
 
 	$scope.reset = function () {
+		$scope.state = '{"info":$index%2==0," ":$index==1}';
+		$('#add option').attr('selected', null);
+		$('#rem option').attr('selected', null);
+		for(i = 0; i < 6; i++){
+			try {
+				document.getElementById("addBut"+i).disabled = false;
+			    document.getElementById("remBut"+i).disabled = false;
+			}
+			catch(err) {
+			    continue;
+			}
+		}
+		$scope.reset_acc_lists = {
+			admin_groups: ["infrastructure_sg", "support_sg", "networking_sg"],
+			env_tag: ["prod", "dev"],
+			allowed_groups: ["infrastructure_sg", "support_sg", "networking_sg"],
+			allowed_users: ["splunksvc"],
+			roles: ["admin", "nadmin"],
+			role : ["admin", "nadmin"]
+		};
 		$scope.existing_lists = JSON.parse(reset_vals);
+		for (var k in $scope.existing_lists){
+			if ($scope.existing_lists[k].constructor === Array){
+				for(var nested in $scope.existing_lists[k]){
+					if ($scope.acc_lists[k].indexOf($scope.existing_lists[k][nested])>=0){
+						$scope.acc_lists[k].splice($scope.acc_lists[k].indexOf($scope.existing_lists[k][nested]), 1);
+					}
+				}
+			}
+		}
+		for (var k in $scope.reset_acc_lists){
+			for(var nested in $scope.reset_acc_lists[k]){
+				if ($scope.existing_lists[k].indexOf($scope.reset_acc_lists[k][nested])<0 && $scope.acc_lists[k].indexOf($scope.reset_acc_lists[k][nested])<0){
+					$scope.acc_lists[k].push($scope.reset_acc_lists[k][nested]);
+				}
+			}
+		}
 	}
 
 	//ensures assets are not added to table multiple times by pressing back button
